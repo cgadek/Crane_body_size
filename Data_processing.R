9051379c17f73304b23b84a4679853f7ce577577
The horse <thehorse@thes-mbp.unm.edu>
2023-05-12 12:27:03 -0600

    Update pretty much everything. Streamlining data processing.

--- Data_processing.R
+++ Data_processing.R
@@ -1,5 +1,6 @@
 ### Crane Body Size Data Processing File ####
 
+
 library(tidyverse)
 library(readr)
 library(ggplot2)
@@ -10,13 +11,9 @@ morpho_massive <-
   read_csv("~/Dropbox/Research/Crane_Trachea/data/Crane harvest data-WORKING COPY.csv")
 
 
-morpho_big <- read.csv("~/Dropbox/Research/Crane_Trachea/data/morpho_large.csv", header = T)
-
-morpho_big <- morpho_big %>%
-  dplyr::rename(taxon.name = taxon_name,
-                posterior.culmen = posterior_culmen) %>%
-  mutate(data.origin = "MSB")
-
+# morpho_big <-
+#   read.csv("~/Dropbox/Research/Crane_Trachea/data/morpho_large.csv",
+#            header = T)
 
 morpho_massive <- morpho_massive %>%
   dplyr::select(
@@ -34,8 +31,9 @@ morpho_massive <- morpho_massive %>%
     Age,
     `Trachea length`,
     `Trachea diameter`,
+    Collector
   ) %>%
-  filter(County %in% c("SOCORRO", "VALENCIA", "Socorro- 2", "2", "3")) %>%
+  filter(Age %in% c("ADULT", "IMMATURE")) %>%
   dplyr::rename(
     mean_trachea_length = `Trachea length`,
     mass = Weight,
@@ -49,6 +47,7 @@ morpho_massive <- morpho_massive %>%
     sex = Sex
   ) %>%
   mutate(
+    row.id = seq.int(nrow(.)),
     mass = as.numeric(mass),
     tail = as.numeric(tail),
     wing.chord = as.numeric(wing.chord),
@@ -85,28 +84,184 @@ morpho_massive <- morpho_massive %>%
     sex = if_else(sex == "MALE", "M", if_else(
       sex == "m", "M", if_else(sex == "FEMALE", "F", if_else(sex == "f", "F", sex))
     )),
-    data.origin = "NMDGF"
-  ) %>%
-  bind_rows(., morpho_big)%>%
-  dplyr::rename(
-    taxon_name = taxon.name)%>%
-  mutate(mass = ifelse(taxon_name == "Antigone canadensis canadensis" & mass < 4000 & taxon_name == "Antigone canadensis canadensis" & mass > 2500 | 
-           taxon_name == "Antigone canadensis tabida" & mass > 4000 & taxon_name == "Antigone canadensis tabida" & mass < 7000, mass, NA),
-         wing.chord = ifelse(taxon_name == "Antigone canadensis canadensis" & wing.chord < 510 & taxon_name == "Antigone canadensis canadensis" & wing.chord > 410 | 
-                               taxon_name == "Antigone canadensis tabida" & wing.chord > 470 & taxon_name == "Antigone canadensis tabida" & wing.chord < 580, wing.chord, NA),
-         tarsus = ifelse(taxon_name == "Antigone canadensis canadensis" & tarsus < 220 & taxon_name =="Antigone canadensis canadensis" & tarsus > 150 | 
-                               taxon_name == "Antigone canadensis tabida" & tarsus > 200 & taxon_name == "Antigone canadensis tabida" & tarsus < 270, tarsus, NA),
-         tail = ifelse(taxon_name == "Antigone canadensis canadensis" & tail < 190 & taxon_name =="Antigone canadensis canadensis" & tail > 140 | 
-                           taxon_name == "Antigone canadensis tabida" & tail > 170 & taxon_name == "Antigone canadensis tabida" & tail < 230, tail, NA),
-         anterior.culmen = ifelse(taxon_name == "Antigone canadensis canadensis" & anterior.culmen < 70 & taxon_name =="Antigone canadensis canadensis" & anterior.culmen > 50 | 
-                         taxon_name == "Antigone canadensis tabida" & anterior.culmen > 70 & taxon_name == "Antigone canadensis tabida" & anterior.culmen < 100, anterior.culmen, NA),
-        posterior.culmen = ifelse(taxon_name == "Antigone canadensis canadensis" & posterior.culmen < 85 & taxon_name =="Antigone canadensis canadensis" & posterior.culmen > 55 | 
-                                    taxon_name == "Antigone canadensis tabida" & posterior.culmen > 85 & taxon_name == "Antigone canadensis tabida" & posterior.culmen < 120, posterior.culmen, NA),
-        wt.index = wing.chord/tarsus)
+    County = if_else(
+      County %in% c("Socorro- 2", "2"),
+      "SOCORRO",
+      if_else(
+        County %in% c("3"),
+        "VALENCIA",
+        if_else(County %in% c("Dona Ana"), "DONA ANA", County)
+      )
+    ),
+    Collector = if_else(Collector == "Barry Hale", "BARRY HALE", if_else(Collector %in% c("G. SCHMITT", "C. Gregory Schmitt", "GREG SCHMITT"), "C. GREGORY SCHMITT", if_else(Collector =="CARD BILL SURVEY", "BILL CARD SURVEY", if_else(Collector == "Janell M. Ward", "JAN WARD", if_else(Collector == "KIM", "KIM FALZONE", if_else(Collector %in% c("TIM MITCHUSOSN", "Tim Mitchusson"), "TIM MITCHUSSON", if_else(is.na(Collector), "UNKNOWN", Collector)))))))) %>%
+      dplyr::rename(taxon_name = taxon.name) %>%
+      mutate(
+        mass = ifelse(
+          taxon_name == "Antigone canadensis canadensis" &
+            mass < 4000 &
+            taxon_name == "Antigone canadensis canadensis" &
+            mass > 2500 |
+            taxon_name == "Antigone canadensis tabida" &
+            mass > 4000 &
+            taxon_name == "Antigone canadensis tabida" &
+            mass < 7000,
+          mass,
+          NA
+        ),
+        wing.chord = ifelse(
+          taxon_name == "Antigone canadensis canadensis" &
+            wing.chord < 510 &
+            taxon_name == "Antigone canadensis canadensis" &
+            wing.chord > 410 |
+            taxon_name == "Antigone canadensis tabida" &
+            wing.chord > 470 &
+            taxon_name == "Antigone canadensis tabida" &
+            wing.chord < 580,
+          wing.chord,
+          NA
+        ),
+        tarsus = ifelse(
+          taxon_name == "Antigone canadensis canadensis" &
+            tarsus < 220 &
+            taxon_name == "Antigone canadensis canadensis" &
+            tarsus > 150 |
+            taxon_name == "Antigone canadensis tabida" &
+            tarsus > 200 &
+            taxon_name == "Antigone canadensis tabida" &
+            tarsus < 270,
+          tarsus,
+          NA
+        ),
+        tail = ifelse(
+          taxon_name == "Antigone canadensis canadensis" &
+            tail < 190 &
+            taxon_name == "Antigone canadensis canadensis" &
+            tail > 140 |
+            taxon_name == "Antigone canadensis tabida" &
+            tail > 170 &
+            taxon_name == "Antigone canadensis tabida" & tail < 230,
+          tail,
+          NA
+        ),
+        anterior.culmen = ifelse(
+          taxon_name == "Antigone canadensis canadensis" &
+            anterior.culmen < 70 &
+            taxon_name == "Antigone canadensis canadensis" &
+            anterior.culmen > 50 |
+            taxon_name == "Antigone canadensis tabida" &
+            anterior.culmen > 70 &
+            taxon_name == "Antigone canadensis tabida" &
+            anterior.culmen < 100,
+          anterior.culmen,
+          NA
+        ),
+        posterior.culmen = ifelse(
+          taxon_name == "Antigone canadensis canadensis" &
+            posterior.culmen < 85 &
+            taxon_name == "Antigone canadensis canadensis" &
+            posterior.culmen > 55 |
+            taxon_name == "Antigone canadensis tabida" &
+            posterior.culmen > 85 &
+            taxon_name == "Antigone canadensis tabida" &
+            posterior.culmen < 120,
+          posterior.culmen,
+          NA
+        ),
+        wt.index = wing.chord / tarsus, #make index proportion wing/tarsus length
+        wm.index = wing.chord / (mass^(1/3)) #make index proportion wing/mass^1/3
+      ) %>%
+      filter(
+        County %in% c("SOCORRO", "VALENCIA", "DONA ANA", "LUNA", "SIERRA", "TORRANCE"),
+        taxon_name %in% c("Antigone canadensis tabida", "Antigone canadensis canadensis"),
+        Year %in% c(1983:2010),
+        sex %in% c("M", "F")
+      )
 
-mm.g <- morpho_massive%>%filter(taxon_name =="Antigone canadensis tabida", sex %in% c("M", "F"))
-mm.l <- morpho_massive%>%filter(taxon_name =="Antigone canadensis canadensis", sex %in% c("M", "F"))
+# determine sex ratio by year,  separately
 
+sex_ratio <- morpho_massive%>%
+  dplyr::select(taxon_name, Year, sex)%>%
+  group_by(taxon_name,  Year, sex)%>%
+  dplyr::summarise(n = n())%>%
+  ungroup()%>%
+  group_by(taxon_name, Year)%>%
+  pivot_wider(names_from = sex, values_from = n) %>%
+  mutate(
+         ratio = M/`F`)
+
+  
+morpho_massive <- morpho_massive%>%
+  left_join(., sex_ratio, by = c("Year", "taxon_name"))
+    
+  
+mm.g <-
+  morpho_massive %>% filter(taxon_name == "Antigone canadensis tabida", sex %in% c("M", "F"))
+mm.l <-
+  morpho_massive %>% filter(taxon_name == "Antigone canadensis canadensis",
+                            sex %in% c("M", "F"))
+
+
+# PCA dataset. This will be inherently smaller given missing data
+
+select.cols.big<-c(1, 3:6, 15, 22, 12, 8:11) #select all non measure columns you want in final dataset
+select.cols.little<-c(8:11)
+
+Years <- morpho_massive%>%  #list of years for which we will organize pc data
+  filter(!Year%in% c(1983, 1984, 1986, 1987, 2002))%>%
+  dplyr::select(Year)%>%
+  distinct()%>%
+  pull(Year)
+
+pca.years <- replicate(length(Years), vector("list", 2), simplify = FALSE)
+
+
+for(i in 1:length(pca.years)){ 
+  
+  pca.cols.to.add <-morpho_massive%>%
+    filter(sex %in% c("M", "F"),
+           Year == Years[i],
+           taxon_name %in% c("Antigone canadensis tabida", "Antigone canadensis canadensis"))%>%
+    dplyr::select(all_of(select.cols.big))%>%
+    filter_at(vars(9:12), all_vars(!is.na(.)))%>%
+    dplyr::select(1:8)
+  
+  
+  pca.f <-morpho_massive%>%
+    filter(sex %in% c("M", "F"),
+           Year == Years[i],
+           taxon_name %in% c("Antigone canadensis tabida", "Antigone canadensis canadensis"))%>%
+    dplyr::select(all_of(select.cols.little))%>%
+    na.omit()%>%
+    prcomp(., center=T,
+           scale=T)
+  
+  
+  pca.data<- as.data.frame(pca.f$x)
+  
+  pca.data$taxon_name <- pca.cols.to.add$taxon_name
+  pca.data$sex <- pca.cols.to.add$sex
+  pca.data$Year <- pca.cols.to.add$Year
+  pca.data$County <- pca.cols.to.add$County
+  pca.data$sex.ratio <- pca.cols.to.add$ratio
+  pca.data$mass <- pca.cols.to.add$mass
+  pca.data$collector <- pca.cols.to.add$Collector
+  pca.data$Age <- pca.cols.to.add$Age
+  
+  pca.years[[i]][[1]] <- pca.data
+  
+  pca.years[[i]][[2]] <- pca.f
+  
+}
+
+
+names(pca.years) <- Years
+
+pca.score.years <- pca.years[[1]][[1]]
+
+for(i in 2:length(Years)){
+  temp <- pca.years[[i]][[1]]
+  pca.score.years <- rbind(pca.score.years, temp)
+}
 
 ### Examine distributions
 
@@ -141,9 +296,16 @@ hist(mm.l$anterior.culmen) #removed above 70 and below 50
 hist(mm.g$posterior.culmen) #removed below 85 and above 120
 hist(mm.l$posterior.culmen) #removed above 85 and below 55
 
+
+hist(pca.data%>%filter(taxon_name =="Antigone canadensis tabida")%>%dplyr::select(PC1)%>%pull())
+hist(pca.data%>%filter(taxon_name =="Antigone canadensis canadensis")%>%dplyr::select(PC1)%>%pull())
+
 #write out separate datasets
 
 
 
 write.csv(mm.g, "data/greater_body_size.csv")
 write.csv(mm.l, "data/lesser_body_size.csv")
+write.csv(morpho_massive, "data/morpho_massive.csv")
+write.csv(pca.data, "data/pca_combined_subspecies.csv")
+
--- functions/cbs_model_runners.R
+++ functions/cbs_model_runners.R
@@ -106,7 +106,32 @@ run_my_cbs_models <- function(data, name, part, seed) {
     #adapt_delta the target average proposal acceptance probability during Stan's adaptation period
     seed = seed
   )
+  
   f5 <- brm(
+    formula = bf(paste0(part, "~ 1 + Year + sex + (1|County)")),
+    #autocor = cor_ar(~1, p = 1),
+    data = data,
+    family = student(link="identity"),
+    cores = 8,
+    chains = 4,
+    thin = 10,
+    #chop out transitions?
+    warmup = 5000,
+    #half of iterations
+    iter = 10000,
+    # prior = c(
+    #   prior(student_t(3, 2.8, 2.5), "Intercept"),
+    #   prior(normal(0, 5), class = "b")
+    # ),
+    #prior = priors,
+    save_pars = save_pars(all = TRUE),
+    #need this for loo comparison
+    control = list(adapt_delta = 0.97, max_treedepth = 17),
+    #adapt_delta the target average proposal acceptance probability during Stan's adaptation period
+    seed = seed
+  )
+  
+  f6 <- brm(
     formula = bf(paste0(part, "~ 1 + Year + sex + Year * sex")),
     data = data,
     #autocor = cor_ar(~1, p = 1),
@@ -132,8 +157,112 @@ run_my_cbs_models <- function(data, name, part, seed) {
     seed = seed
   )
   
+  f7 <- brm(
+    formula = bf(paste0(part, "~ 1 + Year + sex + Year * sex + Age + sex.ratio + Year * sex.ratio")),
+    data = data,
+    #autocor = cor_ar(~1, p = 1),
+    family = student(link="identity"),
+    cores = 8,
+    chains = 4,
+    thin = 10,
+    #chop out transitions?
+    warmup = 5000,
+    #half of iterations
+    iter = 10000,
+    #prior = c(
+    #     prior(normal(0, 10), "Intercept"),
+    #     prior(cauchy(0, 2.5), "sigma")),
+    # prior = c(
+    #   prior(student_t(3, 2.8, 2.5), "Intercept"),
+    #   prior(normal(0, 5), class = "b")
+    # ),
+    save_pars = save_pars(all = TRUE),
+    #need this for loo comparison
+    control = list(adapt_delta = 0.98, max_treedepth = 17),
+    #adapt_delta the target average proposal acceptance probability during Stan's adaptation period
+    seed = seed
+  )
+  
+  f8 <- brm(
+    formula = bf(paste0(part, "~ 1 + Year + sex + Age + sex.ratio")),
+    data = data,
+    #autocor = cor_ar(~1, p = 1),
+    family = student(link="identity"),
+    cores = 8,
+    chains = 4,
+    thin = 10,
+    #chop out transitions?
+    warmup = 5000,
+    #half of iterations
+    iter = 10000,
+    #prior = c(
+    #     prior(normal(0, 10), "Intercept"),
+    #     prior(cauchy(0, 2.5), "sigma")),
+    # prior = c(
+    #   prior(student_t(3, 2.8, 2.5), "Intercept"),
+    #   prior(normal(0, 5), class = "b")
+    # ),
+    save_pars = save_pars(all = TRUE),
+    #need this for loo comparison
+    control = list(adapt_delta = 0.98, max_treedepth = 17),
+    #adapt_delta the target average proposal acceptance probability during Stan's adaptation period
+    seed = seed
+  )
+  
+  f9 <- brm(
+    formula = bf(paste0(part, "~ 1 + Year + sex + Year * sex + Age + sex.ratio")),
+    data = data,
+    #autocor = cor_ar(~1, p = 1),
+    family = student(link="identity"),
+    cores = 8,
+    chains = 4,
+    thin = 10,
+    #chop out transitions?
+    warmup = 5000,
+    #half of iterations
+    iter = 10000,
+    #prior = c(
+    #     prior(normal(0, 10), "Intercept"),
+    #     prior(cauchy(0, 2.5), "sigma")),
+    # prior = c(
+    #   prior(student_t(3, 2.8, 2.5), "Intercept"),
+    #   prior(normal(0, 5), class = "b")
+    # ),
+    save_pars = save_pars(all = TRUE),
+    #need this for loo comparison
+    control = list(adapt_delta = 0.98, max_treedepth = 17),
+    #adapt_delta the target average proposal acceptance probability during Stan's adaptation period
+    seed = seed
+  )
+  
+  f10 <- brm(
+    formula = bf(paste0(part, "~ 1 + Year + sex + Year * sex + Age + sex.ratio + Year * sex.ratio + (1|County)")),
+    data = data,
+    #autocor = cor_ar(~1, p = 1),
+    family = student(link="identity"),
+    cores = 8,
+    chains = 4,
+    thin = 10,
+    #chop out transitions?
+    warmup = 5000,
+    #half of iterations
+    iter = 10000,
+    #prior = c(
+    #     prior(normal(0, 10), "Intercept"),
+    #     prior(cauchy(0, 2.5), "sigma")),
+    # prior = c(
+    #   prior(student_t(3, 2.8, 2.5), "Intercept"),
+    #   prior(normal(0, 5), class = "b")
+    # ),
+    save_pars = save_pars(all = TRUE),
+    #need this for loo comparison
+    control = list(adapt_delta = 0.98, max_treedepth = 17),
+    #adapt_delta the target average proposal acceptance probability during Stan's adaptation period
+    seed = seed
+  )
+  
   #compare model with LOOIC
-  m.comp<-LOO(f1,f2,f3,f4, f5, moment_match=T)
+  m.comp<-LOO(f1,f2,f3,f4, f5, f6, f7, f8, f9, f10, moment_match=T)
   
   #Get Bayes R^2
   br1<-bayes_R2(f1)
@@ -141,12 +270,17 @@ run_my_cbs_models <- function(data, name, part, seed) {
   br3<-bayes_R2(f3)
   br4<-bayes_R2(f4)
   br5<-bayes_R2(f5)
+  br6<-bayes_R2(f6)
+  br7<-bayes_R2(f7)
+  br8<-bayes_R2(f8)
+  br9<-bayes_R2(f9)
+  br10<-bayes_R2(f10)
   
-  br2L <- list(br1, br2, br3, br4, br5)
+  br2L <- list(br1, br2, br3, br4, br5, br6, br7, br8, br9, br10)
   
-  t <- tibble(dataset = c(rep(name,times=5),"LOO_comp", "BayesR2"),
-                  model_set = c(1:5, "model_compare", "BayesR2"),
-                  m = list(f1, f2, f3, f4, f5, m.comp, br2L))
+  t <- tibble(dataset = c(rep(name,times=10),"LOO_comp", "BayesR2"),
+                  model_set = c(1:10, "model_compare", "BayesR2"),
+                  m = list(f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, m.comp, br2L))
   
   assign(name, t, envir=globalenv())
 } 
